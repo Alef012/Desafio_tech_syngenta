542b1ae315bc304e980c442dae650c41782634ab
Felipe alef <felipealef@gmail.com>
2022-05-10 12:14:51 -0300

    adicionando entrada

--- src/my_module.py
+++ src/my_module.py
@@ -60,7 +60,6 @@ def get_cheapest_hotel(entrada):   #DO NOT change the function's name
     menor=hotel1
     for i in hoteis:
         i.preco=i.calcula_preco(datas,tipo_cliente)
-        print(i.preco)
         if i.preco< menor.preco:
             menor=i
         elif i.preco == menor.preco:
@@ -69,6 +68,4 @@ def get_cheapest_hotel(entrada):   #DO NOT change the function's name
     
     return menor.nome
 
-print(get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
-print(get_cheapest_hotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
-print(get_cheapest_hotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
+print(get_cheapest_hotel(input("Digite a entrada no formato: <tipo_do_cliente>: <data1>, <data2>, <data3>: ")))
